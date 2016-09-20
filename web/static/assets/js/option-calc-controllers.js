1a66feec705253a585fa6af64b0e609bd823ee0b
david grupp <david@grupp.me>
2016-09-19 21:28:50 -0500

    improved stragies list

--- web/static/assets/js/option-calc-controllers.js
+++ web/static/assets/js/option-calc-controllers.js
@@ -1,16 +1,28 @@
 console.log('in option-calc-contollers.js');
-var SetupPositions = function(positions) {
+var SetupPositions = function(strategies) {
 	console.log('in SetupPositions');
 	angular.module("root",[]).controller("StrategyController", function ($scope, $http) {
 		console.log('in StrategyController');		
 		var self = this;
-		self.positions = positions;
+
+		self.positions = strategies.strategies[0].positions;
+
+		var totalPrice = self.positions
+				.map(p => p.quantity > 0 ? p.price : -1 * p.price )
+				.reduce((p, a ) => p + a)
+				.toFixed(2);
+
+		
+		self.total = { price: totalPrice };
 
 		self.Action = 'Loading';
 
-		$('#BtnRemove').click(function (position) {
+		$('.BtnRemove').click(function (elm) {
 			//self.Action = 'List';
 			//$scope.$apply();
+			console.log('asdf');
+			var classes = $(elm).attr('class').split(/\s+/);
+			console.log(classes);
 		});
 
 		/*ReadStrategy($scope, $http, function () {
