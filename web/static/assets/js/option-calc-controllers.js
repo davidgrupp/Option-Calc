c750bac87c05fe4c0d8a08108d1a9818dd83fa11
david grupp <david@grupp.me>
2016-09-20 23:31:48 -0500

    updated controller to respond to events

--- web/static/assets/js/option-calc-controllers.js
+++ web/static/assets/js/option-calc-controllers.js
@@ -1,11 +1,16 @@
-console.log('in option-calc-contollers.js');
+
 var SetupPositions = function(strategies) {
-	console.log('in SetupPositions');
 	angular.module("root",[]).controller("StrategyController", function ($scope, $http) {
 		console.log('in StrategyController');		
 		var self = this;
-
+		console.log("settings: " + JSON.stringify(strategies.settings));
+		self.chartSettings = strategies.settings;
 		self.positions = strategies.strategies[0].positions;
+		for(var i = 0; i < self.positions.length; i++){
+			console.log(self.positions[i]);
+			//if(self.positions[i] != null)
+			self.positions[i].id = i;
+		}
 
 		var totalPrice = self.positions
 				.map(p => p.quantity > 0 ? p.price : -1 * p.price )
@@ -25,15 +30,28 @@ var SetupPositions = function(strategies) {
 			console.log(classes);
 		});
 
+		self.RemovePosition = function(position){
+			alert('remove' + JSON.stringify(position));
+			self.positions.splice(position.id, 1);
+		};
+
 		/*ReadStrategy($scope, $http, function () {
 			self.Action = 'List';
 
 			//read positions if this is a saved strategy
 		});*/
 
-		self.addPosition = function (position) {
-			alert(JSON.stringify(position));
-			$scope.data.push(data);
+		self.addPosition = function () {
+			alert("in add position");
+			self.positions.push({"strike": 100, "price": 0.0, "type": "", "quantity": 0});
+		};
+
+		self.updatePositions = function(){
+			var options = self.positions.map(function(p) {
+				return { "strike": p.strike, "price": p.price, "type": p.type, "quantity": p.quantity }; 
+				});
+			console.log("updatePositions settings: " + JSON.stringify(self.chartSettings));
+			SetGraph({ "settings": self.chartSettings, "options": options });
 		};
 
 		//$('#cmpInd').multiselect();
---
+++ web/static/assets/js/option-calc.js
@@ -0,0 +1,85 @@
+
+function SetGraph(chartConfig){
+    console.log("setgraph: \r\n"+JSON.stringify(chartConfig));
+     $.post({
+        url: '/api/chart/points',
+        data: JSON.stringify(chartConfig),
+        contentType: 'application/json',
+        success: function(data){
+            console.log(data);
+            var xdata = [];
+            var ydata = [];
+            for(var i = 0; i < data.chart.length; i++){
+                xdata.push(data.chart[i].x);
+                ydata.push([data.chart[i].x, data.chart[i].y]);
+            }
+
+            //console.log(xdata);
+            //console.log(ydata);
+
+            $('#chart').highcharts({
+                chart: {
+                    type: 'area',
+                    
+                },
+                title: {
+                    text: 'Strategy'
+                },
+                xAxis: {
+                    title: {
+                        enabled: false,
+                        text: "Price"
+                    },
+                    minPadding: 0,
+                    maxPadding: 0,
+                    startOnTick: true,
+                    endOnTick: true,
+                    showLastLabel: false
+                },
+                yAxis: {
+                    title: {
+                        text: "Gain / Loss"
+                    },
+                    minorGridLineWidth: "0px",
+                    plotLines: [{
+                        color: "rgb(90, 90, 90)",
+                        width: 2,
+                        value: 0,
+                        zIndex: 1
+                    }]
+                },
+                credits: {
+                    enabled: false
+                },
+                series: [{
+                    name: 'Strategy 1',
+                    data: ydata
+                }],
+                plotOptions: {
+                    area: {
+                        animation: true,
+                        lineWidth: 3,
+                        allowPointSelect: false,
+                        color: "rgb(90, 180, 90)",
+                        negativeColor: "rgb(180, 90, 90)",
+                        marker: {
+                            radius: 0,
+                            states: {
+                                hover: { enabled: true, lineColor: "rgb(100,0,100)" }
+                            }
+                        },
+                        states: {
+                            hover: {
+                                marker: { enabled: true }
+                            }
+                        },
+                        tooltip: {
+                            headerFormat: "<b>{series.name}<\/b><br>",
+                            pointFormat: "Price = {point.x}<br />Gain / Loss = {point.y}"
+                        }
+                    }
+                }
+            });
+
+        }});
+}
