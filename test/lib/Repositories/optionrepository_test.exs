4d658dcb459a14fb5478482ef82db710d3c1d16f
david grupp <david@kcura.com>
2016-10-25 21:28:39 -0500

    updated tests to query options for a friday in the future

--- test/lib/Repositories/optionrepository_test.exs
+++ test/lib/Repositories/optionrepository_test.exs
@@ -8,8 +8,12 @@ defmodule OptionRepositoryTest do
   end
 
   test "Option Repository read strike prices" do
+    today_erl = DateTime.utc_now |> DateTime.to_date |> Date.to_erl
+    day_of_week = :calendar.day_of_the_week today_erl
+    friday_erl = :calendar.gregorian_days_to_date(:calendar.date_to_gregorian_days(today_erl) + (12 - day_of_week)) # 12 for the friday after next so we can always get a friday in the future
+    next_friday = Date.from_erl! friday_erl
 
-    results = OptionCalc.Repositories.OptionRepository.read_strike_prices("aapl", ~D[2016-10-07]) |> Task.await
+    results = OptionCalc.Repositories.OptionRepository.read_strike_prices("aapl", next_friday) |> Task.await
     assert Enum.count(results) > 3
   end
 end
